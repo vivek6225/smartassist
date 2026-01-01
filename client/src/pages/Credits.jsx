257797611e19e30c85b86ec64acab9da3ce20c03
vivek kumar <vivekkumar06102002@gmail.com>
2026-01-02 02:48:48 +0530

    feat : Add community page and partial credits page

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPlans } from '../assets/assets'
 
 const Credits = () => {
+
+  const [plans, setPlans] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchPlans = async () => {
+    setPlans(dummyPlans)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchPlans()
+  }, [])
+
+  if (loading) return <div>Loading...</div>
+
   return (
-    <div>
-      Credits
+    <div className='max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+      <h2 className='text-3xl font-semibold text-center mb-10 xl:ml-30 text-gray-800 dark:text-white'>
+        Credits Plans
+      </h2>
     </div>
   )
 }
