befb2bd4e0f19d48021d58e9399fd01bda77c832
vivek kumar <vivekkumar06102002@gmail.com>
2026-01-15 02:26:00 +0530

    Add Gemini AI configuration

---
+++ server/configs/genai.js
@@ -0,0 +1,6 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+export default genAI;
--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,18 +1,72 @@
+import Chat from "../models/Chat.js";
+import User from "../models/User.js";
+import genAI from "../configs/gemini.js";
 
-import Chat from "../models/Chat.js"
-
-
-//Text based AI chat message controller
+// Text based AI chat message controller
 export const textMessageController = async (req, res) => {
-  try {
-    const userId = req.user._id
-    const {chatId, prompt} = req.body
+    try {
+        const userId = req.user._id;
+        const { chatId, prompt } = req.body;
 
-     const chat = await Chat.findOne({userId, _id: chatId})
-     chat.messages.push({role: "user", content: prompt, timestamp: Date.now(), isImage: false})
+        const chat = await Chat.findOne({ userId, _id: chatId });
+        if (!chat) return res.json({ success: false, message: "Chat not found" });
 
-  } catch (error) {
-    
-  }
-  
-}
+        chat.messages.push({
+            role: "user",
+            content: prompt,
+            timestamp: Date.now(),
+            isImage: false
+        });
+
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const result = await model.generateContent(prompt);
+        const aiResponse = result.response.text();
+
+        const reply = {
+            role: "assistant",
+            content: aiResponse,
+            timestamp: Date.now(),
+            isImage: false
+        };
+
+        chat.messages.push(reply);
+        await chat.save();
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
+
+        res.json({ success: true, reply });
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+};
+
+// Image Generation message controller
+export const imageMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        if (req.user.credits < 2) {
+            return res.json({ success: false, message: "You don't have enough credits" });
+        }
+
+        const { prompt, chatId , isPublished} = req.body;
+
+        const chat = await Chat.findOne({ userId, _id: chatId });
+        if (!chat) return res.json({ success: false, message: "Chat not found" });
+        
+        // find chat
+
+        chat.messages.push({
+            role: "user",
+            content: prompt,
+            timestamp: Date.now(),
+            isImage: false
+        });
+
+      
+
+    } catch (error) {
+       
+    }
+};
